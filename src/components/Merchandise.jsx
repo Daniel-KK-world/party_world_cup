6273b3e0651b99a7376b601867a3230be31621dc
Daniel P.K <danilofletcher978@gmail.com>
2026-05-28 22:35:26 +0000

    fix: Standardize import statement quotes in Merchandise component

--- src/components/Merchandise.jsx
+++ src/components/Merchandise.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import jerseyImg from '../assets/jersey.png';
-import hatImg from '../assets/hat.png';
-import scarfImg from '../assets/scarf.png';
+import jerseyImg from "../assets/jersey.png";
+import hatImg from "../assets/hat.png";
+import scarfImg from "../assets/scarf.png";
 
 export default function Merchandise() {
   const products = [
